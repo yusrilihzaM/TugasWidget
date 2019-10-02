a744aed83858e7b7edef57168f0653b3d0e87ede
yusrilx02 <mohyusrilihzamaulana@gmail.com>
2019-10-02 13:06:48 +0700

    Menu awal

--- app/src/main/java/com/yusril/tugaswidget/MainActivity.java
+++ app/src/main/java/com/yusril/tugaswidget/MainActivity.java
@@ -2,8 +2,10 @@ package com.yusril.tugaswidget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,84 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1=findViewById(R.id.TextView);
+        Button btn2=findViewById(R.id.ImageView);
+        Button btn3=findViewById(R.id.ImageButton);
+        Button btn4=findViewById(R.id.EditText);
+        Button btn5=findViewById(R.id.CheckBox);
+        Button btn6=findViewById(R.id.RadioButton);
+        Button btn7=findViewById(R.id.DatePicker);
+        Button btn8=findViewById(R.id.TimePicker);
+        Button btn9=findViewById(R.id.SwichButton);
+        Button btn10=findViewById(R.id.DigitalClock);
+        Button btn11=findViewById(R.id.AnalogClock);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetTextView.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetImageView.class));
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetImageButton.class));
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetEditText.class));
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetCheckBox.class));
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetRadioButton.class));
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetDatePicker.class));
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetTimePicker.class));
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetSwitchButton.class));
+            }
+        });
+        btn10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetDigitalClock.class));
+            }
+        });
+        btn11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,WidgetAnalogClock.class));
+            }
+        });
     }
 
 
